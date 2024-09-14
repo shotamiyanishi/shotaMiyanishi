6569cc55804b3bbf8b4c00e9719b9464d2c2fc6e
shotamiyanishi <szbz47zt@gmail.com>
2024-09-15 04:20:43 +0900

    コメントアウト微修正

--- Optimization/benchmarks.py
+++ Optimization/benchmarks.py
@@ -1,11 +1,13 @@
 import numpy as np
 
-#最適解[0, ..., 0]^n, 評価値0.0, 単峰性 (nは次元数) 
+#nは次元数
+
+#最適解[0, ..., 0]^n, 評価値0.0, 単峰性 
 def Sphere(x):
     eval = float (((x) * (x)).sum())
     return eval
 
-#最適解[0, ..., 0]^n, 評価値0, 多峰性 (nは次元数) 
+#最適解[0, ..., 0]^n, 評価値0, 多峰性
 def Rastrigin(x):
     value  = 0
     n = x.shape[0]
@@ -15,7 +17,7 @@ def Rastrigin(x):
     value += 10  * n
     return value
 
-#最適解[1, ..., 1]^n, 評価値0, 変数間依存性 (nは次元数) 
+#最適解[1, ..., 1]^n, 評価値0, 変数間依存性 
 def RozenBlock(x):
     dim = x.shape[0]
     value = 0.0
